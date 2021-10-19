8913275bf30f662b1cc11de766fb3b4ee0f05555
sana1319 <sandra.szwed2211@gmail.com>
2021-10-19 12:21:07 +0200

    commit message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,12 @@ public class Main {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int x;
-        while(true){
+        while(false){
             System.out.println("Wybierz rodzaj figury ktora chcesz narysowac: ");
             System.out.println("1. Prostokat");
             System.out.println("2. Trojkat");
             System.out.println("3. Wyjdz z programu");
             x = scan.nextInt();
-
 
             if (x == 1) {
                 Rectangle rectangle = new Rectangle();
